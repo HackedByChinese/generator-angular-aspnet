bc1c05dfc3b3fa6193418c76cef7bec080b5670f
Mike Grabski <me@mikegrabski.com>
2014-11-17 23:33:47 -0500

    Add (empty) gulp test target

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,7 @@ var gulp = require('gulp');
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+gulp.task('test', function() {
+  // place code for running tests here.
+});
